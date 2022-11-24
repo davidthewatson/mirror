87e15e9ef47898f03d6001ab54e59082fffba8c6
david watson <davidthewatson@gmail.com>
2022-11-24 12:21:55 -0500

    Who needs scheme, delimiter, site, path?

--- mirror.py
+++ mirror.py
@@ -1,16 +1,11 @@
 from requests_html import HTMLSession 
 
 
-def scrape(scheme='https', delimiter='://', site=None, path=None, url=None):
-    url = url or f'{scheme}{delimiter}{site}{path}'
+def scrape( url=None):
     print(url)
     session = HTMLSession()
     r = session.get(url)
     [scrape(url=link) for link in sorted(r.html.absolute_links) if url in link and link != url]
 
 if __name__ =="__main__":
-    scheme = 'https'
-    delimiter = '://'
-    site = 'davidwatson.org'
-    path = '/'
-    scrape(scheme, delimiter, site, path)
+    scrape('https://davidwatson.org/')
